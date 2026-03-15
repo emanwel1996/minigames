f9bb527124692e9ba6861da79c0adfe6a30ace22
Emanwel <emanuelaprendiending@gmail.com>
2026-03-15 15:50:48 +0100

    Create enum.ts

---
+++ enum.ts
@@ -0,0 +1,30 @@
+enum Minigames {
+    //% block="rock paper scissors"
+    RockPaperScissors,
+    //% block="paint"
+    Paint,
+    //% block="minesweeper"
+    Minesweeper,
+    //% block="catch"
+    Catch
+}
+enum TwoPlayerMinigames {
+    //% block="rock paper scissors"
+    RockPaperScissors
+}
+enum Hands {
+    //% block="rock"
+    Rock,
+    //% block="paper"
+    Paper,
+    //% block="scissors"
+    Scissors
+}
+enum Level {
+    //% block="easy"
+    Easy,
+    //% block="medium"
+    Medium,
+    //% block="difficult"
+    Difficult
+}
